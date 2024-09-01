75a439d76ee7eda677ca6096b5244b19e9a83461
Neon <neon@saahild.com>
2024-09-01 14:42:11 -0400

    chore: grub theme - maybe

--- all_config.nix
+++ all_config.nix
@@ -17,9 +17,19 @@ boot.loader.grub.useOSProber = true;
 boot.loader.grub.efiSupport = true;
 boot.loader.efi.canTouchEfiVariables = true;
 boot.loader.efi.efiSysMountPoint = "/boot";
+ boot.loader.grub.theme = pkgs.stdenv.mkDerivation {
+  pname = "distro-grub-themes";
+  version = "3.1";
+  src = pkgs.fetchFromGitHub {
+    owner = "AdisonCavani";
+    repo = "distro-grub-themes";
+    rev = "v3.1";
+    hash = "sha256-ZcoGbbOMDDwjLhsvs77C7G7vINQnprdfI37a9ccrmPs=";
+  };
+  installPhase = "cp -r customize/nixos $out";
+};
   # enable grub (im cooked if this dosent work)
   # @see https://discourse.nixos.org/t/configure-grub-on-efi-system/2926/3
-  networking.hostName = "nixos"; # Define your hostname.
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
